bca5310279c3d6e16a14d311a20e1bb09ef30334
lhoang1910 <hoang03.it@gmail.com>
2024-06-21 14:27:54 +0700

    cors

--- src/main/java/com/security/config/SecurityConfig.java
+++ src/main/java/com/security/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf(AbstractHttpConfigurer::disable)
+				.cors(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(r -> r.requestMatchers("/api/auth/**").permitAll()
 						.anyRequest().authenticated()
 						)
